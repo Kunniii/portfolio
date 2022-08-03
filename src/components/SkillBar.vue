c28938088bdb66c226fbee821b1400518b543b0b
Kunniii <nguyenairport56@gmail.com>
2022-08-03 13:23:21 +0700

    complete SkillBar.vue

--- src/components/SkillBar.vue
+++ src/components/SkillBar.vue
@@ -1,18 +1,32 @@
 <template>
     <div class="skills__data">
         <div class="skills__names">
-            <i class="bx bxl-meta skills__icon"></i>
-            <span class="skills__name">HTML5</span>
+            <i :class="class_icon"></i>
+            <span class="skills__name">{{this.skill_name}}</span>
         </div>
-        <div class="skills__bar skills__percent"></div>
+        <div class="skills__bar skills__percent" :style="style"></div>
         <div>
-            <span class="skills__percentage">95%</span>
+            <span class="skills__percentage">{{ skill_percentage }}%</span>
         </div>
     </div>
 </template>
 
 <script>
-    export default {};
+    export default {
+      props: [
+        "skill_percentage",
+        "skill_icon",
+        "skill_name",
+      ],
+      computed: {
+        style() {
+          return "width:" + this.skill_percentage + "%;";
+        },
+        class_icon() {
+          return "bx skills__icon bxl-" + this.skill_icon;
+        }
+      }
+    };
 </script>
 
 <style>
--- src/components/TheSkills.vue
+++ src/components/TheSkills.vue
@@ -1,34 +1,36 @@
+<!-- Should take a json encoded props and display it -->
+
 <template>
-    <section class="skills section" id="skills">
-        <h2 class="section-title">Skills</h2>
+  <section class="skills section" id="skills">
+    <h2 class="section-title">Skills</h2>
 
-        <div class="skills__container bd-grid">
-            <div>
-                <h2 class="skills__subtitle">Profesional Skills</h2>
-                <p class="skills__text">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Velit optio id vero amet, alias architecto consectetur error
-                    eum eaque sit.
-                </p>
-                <img src="assets/img/work3.jpg" alt="" class="skills__img" />
-            </div>
+    <div class="skills__container bd-grid">
+      <div>
+        <h2 class="skills__subtitle">Profesional Skills</h2>
+        <p class="skills__text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit optio
+          id vero amet, alias architecto consectetur error eum eaque sit.
+        </p>
+        <img src="assets/img/work3.jpg" alt="" class="skills__img" />
+      </div>
 
-            <div>
-                <div class="skill-bars">
-                    <SkillBar />
-                    <SkillBar />
-                    <SkillBar />
-                    <SkillBar />
-                    <SkillBar />
-                </div>
-            </div>
+      <div>
+        <div class="skill-bars">
+          <SkillBar :skill_name="'HTML'" :skill_icon="'html5'" :skill_percentage="50" />
+          <SkillBar :skill_name="'C++'" :skill_icon="'c-plus-plus'" :skill_percentage="20" />
+          <SkillBar :skill_name="'Facebook'" :skill_icon="'meta'" :skill_percentage="60" />
+          <SkillBar :skill_name="'CSS'" :skill_icon="'css3'" :skill_percentage="70" />
+          <SkillBar :skill_name="'JS'" :skill_icon="'javascript'" :skill_percentage="80" />
+          <SkillBar :skill_name="'Python3'" :skill_icon="'python'" :skill_percentage="90" />
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    import SkillBar from "./SkillBar.vue";
-    export default { components: { SkillBar } };
+  import SkillBar from "./SkillBar.vue";
+  export default { components: { SkillBar } };
 </script>
 
 <style></style>
